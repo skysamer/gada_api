3d75c7e66aa485446f655f904eb55758cf5c3d05
isangmin <skyslayer123@naver.com>
2022-11-17 14:16:24 +0900

    fix : CI/CD 파이프라인 작업-9
    배포자동화 테스트용 커밋-9

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=gada_api
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git pull"
+
+git pull
+
+echo "> 프로젝트 Build 시작"
+
+./gradlew build -x test
+
+echo "> 디렉토리 이동"
+
+cd $REPOSITORY
+
+echo "> build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 PID 확인"
+
+CURRENT_PID=$(pgrep -f gada_api.jar)
+
+echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+nohup java -jar -Dspring.config.location=$REPOSITORY/application.yml $REPOSITORY/$JAR_NAME 2>&1 &
