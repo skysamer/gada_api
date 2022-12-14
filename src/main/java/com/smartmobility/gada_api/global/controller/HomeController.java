b95c6db22a2d769a65211d5f84f5cc0638958364
isangmin <skyslayer123@naver.com>
2022-12-14 16:10:17 +0900

    fix : 토큰 유지시간 변경
    토큰 유지시간 변경

--- src/main/java/com/smartmobility/gada_api/global/controller/HomeController.java
+++ src/main/java/com/smartmobility/gada_api/global/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
     private String port;
     @GetMapping("/")
     public String home() {
-        return "hello gada 0.1.20";
+        return "hello gada 0.1.21";
     }
 
     @GetMapping("/port")
